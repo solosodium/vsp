0dfa9d42807820ca9f04c9a6dc3ef71499df47be
Hao Fu <fuhaoee@gmail.com>
2017-01-18 00:14:11 -0500

    Working on update sandbox

--- test/test.js
+++ test/test.js
@@ -8,6 +8,7 @@ var Rule = require("../lib/rule.js");
 //var r2 = new Rule({});
 //var r3 = new Rule({'vector':'v', 'condition':'c', 'execution':'e'});
 //var r4 = new Rule({'vector':'v', 'condition':'c', 'execution':'e', 'schema':{'a':{}}});
+//console.log(r4);
 
 /**
  * Tests for State
@@ -26,4 +27,15 @@ var State = require("../lib/state.js");
 //s5.removeRule('v2');
 //console.log(s5.getRules());
 
+/**
+ * Tests for Vector
+ */
 
+var Vector = require("../lib/vector.js");
+
+//var v1 = new Vector();
+//var v2 = new Vector({});
+//var v3 = new Vector({vector: 'v'});
+//var v4 = new Vector({data: {}});
+//var v5 = new Vector({vector: 'v', data: {}});
+//console.log(v5);
